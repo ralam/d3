343d78fa8fbff3a9f894be81c0596a9b437af55d
ralam <rehan.alam@gmail.com>
2015-11-03 11:25:51 -0800

    basic histogram

--- graphs.js
+++ graphs.js
@@ -1,3 +1,11 @@
 (function() {
-  d3.select('body').append('p').text('new paragraph');
-})
+  var dataset = [ 25, 7, 5, 26, 11, 8, 25, 14, 23, 19,
+                  14, 11, 22, 29, 11, 13, 12, 17, 18, 10,
+                  24, 18, 25, 9, 3 ];
+  d3.select('body').selectAll('p').data(dataset)
+    .enter()
+    .append('div')
+    .attr('class', 'bar')
+    .style('height', function(d) {return d * 5 + "px"})
+    .style('margin-right', '2px');
+})();
