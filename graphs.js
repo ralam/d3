77310d085ea6650eae219295b8e141e4dd519bc5
ralam <rehan.alam@gmail.com>
2015-11-03 12:05:16 -0800

    rewrite barchart using SVG

--- graphs.js
+++ graphs.js
@@ -1,20 +1,23 @@
 (function() {
   var dataset = [];
-  var height = 50;
-  var width = 500;
-  for(var i = 0; i < 5; i++) {
-    dataset.push(Math.ceil(Math.random() * 10));
+  var svgHeight = 100;
+  var svgWidth = 500;
+  var barPadding = 1;
+  for(var i = 0; i < 25; i++) {
+    dataset.push(Math.ceil(Math.random() * 30));
   };
 
   var svg = d3.select('body').append('svg')
-              .attr('width', width)
-              .attr('height', height);
+              .attr('width', svgWidth)
+              .attr('height', svgHeight);
 
-  var circles = svg.selectAll('circle').data(dataset)
+  var bars = svg.selectAll('rect')
+    .data(dataset)
     .enter()
-    .append('circle');
-
-  circles.attr("cx", function(d, i) {return (i * 50) + 25;})
-    .attr('cy', height/2)
-    .attr('r', function(d) {return d;});
+    .append('rect')
+    .attr('x', function(d, i) {return i * (svgWidth / dataset.length);})
+    .attr('y', function(d) {return svgHeight - d * 4;})
+    .attr('fill', 'steelblue')
+    .attr('width', svgWidth / dataset.length - barPadding)
+    .attr('height', function (d) {return d * 4;});
 })();
