baca2444ed495800f5fac8f9a03804db0f8b1b7b
ralam <rehan.alam@gmail.com>
2015-11-04 18:04:57 -0800

    add listener on form

--- scripts/chartDisplay.js
+++ scripts/chartDisplay.js
@@ -2,4 +2,21 @@
   if (typeof Display === 'undefined') {
     window.Display = {};
   };
+
+  var Editor = Display.Editor = function() {
+    // var piechart = Display.Piechart();
+    var $form = $('form');
+    var $submit = $('button');
+    var data = [];
+
+    Editor.prototype.listenForSubmit = function() {
+      $form.submit(function(e) {
+        e.preventDefault();
+        debugger;
+        data = $form.serializeArray();
+        console.log(data)
+      })
+    };
+
+  };
 })();
